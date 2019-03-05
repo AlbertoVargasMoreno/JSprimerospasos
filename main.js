589708947e6dfc81be48ff35847bd9a4c7d8e8ec
Beto <vargasmorenoalberto@gmail.com>
2019-03-05 17:05:47 -0600

    la version final

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+
 function isEven(number) {
-    return !number%2;
+    return (number%2 === 0);
 }
 
 function factorial(number) {
-
     if(number === 0)
         return 1;
  
@@ -11,4 +11,12 @@ function factorial(number) {
         number *= i;
     }
     return number;
+}
+
+function kebabToSnake(frase) {
+    if(typeof frase !== "string") {
+        return "error, intenta escribir con '' ";
+    }
+    var fraseRenovada = frase.replace(/-/g,"_");
+    return fraseRenovada;
 }
